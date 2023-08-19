eaaa3fddf4e93073ca0989e67281b4b4f1ac5479
Ody Chen <ptp_ody@photons.com.tw>
2023-08-19 14:42:22 +0800

    temp

--- main.go
+++ main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"log"
 	_ "tsEngine/tsFuzzy"
 	_ "tsEngine/tsMap"
 	_ "tsEngine/tsSlice"
 	_ "tsEngine/tsString"
-	"tsEngine/tsTime"
 	_ "tsEngine/tsTime"
 )
 
 func main() {
-	// log.Println(tsTime.MakeTimeSe(int64()))
-	log.Println(tsTime.CurrNs())
+
 }
